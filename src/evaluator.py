25298f0ec66e48b001d975f30bd13d2a26086084
yairf <yairf11@cs.technion.ac.il>
2020-06-30 23:16:42 +0300

    in progress

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,5 +1,27 @@
+import functools
 from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import Tuple
+
+
+@functools.total_ordering
+class Metric:
+    def __init__(self, name: str, value: float, higher_is_better: bool):
+        self.name = name
+        self.value = value
+        self.higher_is_better = higher_is_better
+
+    def _check_other_metric_compatibility(self, other):
+        if other.name != self.name or other.higer_is_better != self.higher_is_better:
+            raise TypeError(f"Comparing two different metrics: {self.name} and {other.name}")
+
+    def __eq__(self, other):
+        self._check_other_metric_compatibility(other)
+        return other.value == self.value
+
+    def __lt__(self, other):
+        self._check_other_metric_compatibility(other)
+        return (self.value < other.value) and self.higher_is_better
 
 
 class Evaluator(ABC):
@@ -7,5 +29,5 @@ class Evaluator(ABC):
         self.ground_truth_file = ground_truth_file
 
     @abstractmethod
-    def evaluate(self):
+    def evaluate(self) -> Tuple[Metric]:
         pass
--- src/submissions_manager.py
+++ src/submissions_manager.py
@@ -1,24 +1,42 @@
+from io import BytesIO, StringIO
 from pathlib import Path
-from typing import List, Callable
+from typing import List, Callable, Tuple, Dict, Union
+
+from src.evaluator import Metric
 
 
 class SingleParticipantSubmissions:
-    def __init__(self, participant_submission_dir: Path, evaluator: Callable[[Path], float]):
+    def __init__(self, participant_submission_dir: Path, evaluator: Callable[[Path], Tuple[Metric]]):
         self.participant_submission_dir = participant_submission_dir
         self.participant_name = self.participant_submission_dir.parts[-1]
         self.evaluator = evaluator
+        self.results : Dict[Path: Tuple[Metric]] = dict()
 
     def get_submissions(self) -> List[Path]:
         return [x for x in self.participant_submission_dir.iterdir() if x.is_file()]
 
-    def
+    def add_submission(self, io_stream: Union[BytesIO, StringIO]):
+
+
+    def clear_results(self):
+        self.results.clear()
+
+    def update_results(self):
+        submissions = self.get_submissions()
+        for submission in submissions:
+            if submission in self.results:
+                continue
+            self.results[submission] = self.evaluator(submission)
+
+    def get_best_result(self) -> Tuple[Path, Tuple[Metric]]:
+        return max([(path, result) for path, result in self.results.items()], key=lambda x: x[1])
 
 
 class SubmissionManager:
     def __init__(self, submissions_dir: Path):
         self.submissions_dir = submissions_dir
 
-    def _create_dir(self):
+    def _create_submissions_dir(self):
         self.submissions_dir.mkdir(parents=True, exist_ok=True)
 
-    def add_
+    def add_participant(self):
