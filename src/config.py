f6cceb63c3537d2d34e672f32028f275f9824a11
yairf <yairf11@cs.technion.ac.il>
2020-07-15 19:07:30 +0300

    updated config.py

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ SUBMISSIONS_DIR = Path(__file__).parent.parent.absolute() / 'submissions'
 PASSWORDS_FILE = Path(__file__).parent.parent.absolute() / 'passwords.json'
 ARGON2_KWARGS = {}
 
-ALLOWED_SUBMISSION_FILE_EXTENSION = 'json'
+ALLOWED_SUBMISSION_FILE_EXTENSION = 'json' # Can be None if any file type is allowed
 
 EVALUATOR_CLASS = ExampleEvaluator
 EVALUATOR_KWARGS = {}
